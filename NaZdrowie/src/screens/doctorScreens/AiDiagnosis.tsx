13258ef6429816390252c76ac8dec4a539573370
DroppedHard <szyper7@gmail.com>
2024-11-09 04:54:58 +0100

    Add new hooks to AiDiagnosis and AllPatientsScreen pages.

--- NaZdrowie/src/screens/doctorScreens/AiDiagnosis.tsx
+++ NaZdrowie/src/screens/doctorScreens/AiDiagnosis.tsx
@@ -1,68 +1,64 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "App";
 import { LinkButton, PrimaryButton } from "components/atoms";
-import { AiAnalysisResultCard, ListCard, Navbar } from "components/molecules";
+import {
+  AiAnalysisResultCard,
+  ListCard,
+  LoadingCard,
+  Navbar,
+} from "components/molecules";
+import { UserContext } from "components/organisms/context";
 import { mainStyle } from "properties/styles";
-import { PatientData, PatientResult, ListCardElement } from "properties/types";
-import React, { useEffect, useState } from "react";
+import { PatientResult } from "properties/types";
+import React, { useContext } from "react";
 import { ScrollView, View } from "react-native";
-import { getPatient, getResults } from "services/patientData";
+import { useFetchPatient, useFetchResults } from "services/patientData";
 
 export const AiDiagnosis = ({
   route,
   navigation,
 }: NativeStackScreenProps<RootStackParamList, "AiDiagnosis">) => {
   const { patientId } = route.params;
-  const [resultsData, setResultssData] = useState<ListCardElement[]>([]);
-  const [patientData, setPatientData] = useState<PatientData>(null);
+  const { currentUser } = useContext(UserContext);
 
-  useEffect(() => {
-    setResults(patientId);
-    setPatient(patientId);
-  }, []);
+  const results = useFetchResults(
+    currentUser,
+    (data) => data.map(formatResultsData),
+    patientId,
+  );
+  const patient = useFetchPatient(currentUser, null, patientId);
 
-  const setResults = async (patientId: number) => {
-    try {
-      const data = await getResults(patientId);
-      const formattedResults = data.map(formatResultsData);
-      setResultssData(formattedResults);
-    } catch (error) {
-      console.error("Error fetching latest patients:", error);
-    }
-  };
-
-  const formatResultsData = (result: PatientResult) => ({
-    checkbox: {
-      checkboxStatus: true, // TODO
-    },
-    text: result.testType,
-    buttons: [<LinkButton title="Podgląd" />],
-  });
-
-  const setPatient = async (patientId: number) => {
-    try {
-      const data = await getPatient(patientId);
-      setPatientData(data);
-    } catch (error) {
-      console.error("Error fetching latest patients:", error);
-    }
-  };
+  function formatResultsData(result: PatientResult) {
+    return {
+      checkbox: {
+        checkboxStatus: true, // TODO
+      },
+      text: result.testType,
+      buttons: [<LinkButton title="Podgląd" />],
+    };
+  }
 
   // const startAiDiagnosis = () => {};
 
   return (
     <View style={{ flex: 1 }}>
-      <Navbar
-        navbarDescriptionTitle={
-          `${patientData?.name} ${patientData?.surname}` || ""
-        }
-      />
-      <ScrollView contentContainerStyle={mainStyle.container}>
-        <ListCard
-          title="Załączone badania"
-          data={resultsData}
-          extraButton={<PrimaryButton title="Poproś AI o analizę" />}
+      {patient.isSuccess ? (
+        <Navbar
+          navbarDescriptionTitle={`${patient.data.name} ${patient.data.surname}`}
         />
+      ) : (
+        <Navbar navbarDescriptionTitle="..." /> // maybe loading here or sth idk
+      )}
+      <ScrollView contentContainerStyle={mainStyle.container}>
+        {results.isSuccess ? (
+          <ListCard
+            title="Załączone badania"
+            data={results.data}
+            extraButton={<PrimaryButton title="Poproś AI o analizę" />}
+          />
+        ) : (
+          <LoadingCard title="Załączone badania" />
+        )}
         <AiAnalysisResultCard />
       </ScrollView>
     </View>
--- NaZdrowie/src/screens/doctorScreens/AllPatientsScreen.tsx
+++ NaZdrowie/src/screens/doctorScreens/AllPatientsScreen.tsx
@@ -1,24 +1,27 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "App";
-import { UserButton, UserButtonElement } from "components/atoms";
-import { Navbar } from "components/molecules";
+import { UserButton } from "components/atoms";
+import { LoadingCard, Navbar } from "components/molecules";
 import { UserContext } from "components/organisms/context";
 import { mainStyle } from "properties/styles";
-import React, { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import { View, ScrollView } from "react-native";
-import { getAllPatients } from "services/doctorData";
+import { useFetchAllPatients } from "services/doctorData";
 
 export const AllPatientsScreen = ({
   navigation,
 }: NativeStackScreenProps<RootStackParamList, "AllPatients">) => {
   const { currentUser } = useContext(UserContext);
-  const [allPatientsData, setAllPatientsData] = useState<UserButtonElement[]>(
-    [],
-  );
 
-  useEffect(() => {
-    setAllPatients(currentUser.id);
-  }, []);
+  const allPatients = useFetchAllPatients(currentUser, (data) => {
+    return data.map((patient) => (
+      <UserButton
+        key={patient.id}
+        title={patient.name}
+        handleOnClick={() => navigateToPatientScreen(patient.id)}
+      />
+    ));
+  });
 
   const navigateToPatientScreen = (patientId: number) => {
     navigation.navigate("PatientDetails", {
@@ -26,24 +29,11 @@ export const AllPatientsScreen = ({
     });
   };
 
-  const setAllPatients = (doctorId: number) =>
-    getAllPatients(doctorId).then((data) => {
-      setAllPatientsData(
-        data.map((patient) => (
-          <UserButton
-            key={patient.id}
-            title={patient.name}
-            handleOnClick={() => navigateToPatientScreen(patient.id)}
-          />
-        )),
-      );
-    });
-
   return (
     <View style={{ flex: 1 }}>
       <Navbar navbarDescriptionTitle="Moi pacjenci" />
       <ScrollView contentContainerStyle={mainStyle.container}>
-        {allPatientsData}
+        {allPatients.isSuccess ? allPatients.data : <LoadingCard />}
       </ScrollView>
     </View>
   );
