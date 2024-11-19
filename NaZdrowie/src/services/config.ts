3a90aa2de531dd6f017aaf6ce3a48754ecef43ac
DroppedHard <szyper7@gmail.com>
2024-11-19 01:52:03 +0100

    Add new AI API url - change when developing!

--- NaZdrowie/src/services/config.ts
+++ NaZdrowie/src/services/config.ts
@@ -1,4 +1,5 @@
 export const API_URL: string = "http://localhost:8080/api/";
+export const AI_API_URL: string = "http://localhost:8081/api/";
 
 export const latestCount: number = 3;
 export const cardCommentsCount: number = 1;
