f4bec23f069827d658f3ebb9ce5977b2035f8923
Olciaaa <ola.poskrobek@gmail.com>
2024-12-15 13:34:48 +0100

    input multiline enabled

--- NaZdrowie/src/components/atoms/PersonalizedTextInput.tsx
+++ NaZdrowie/src/components/atoms/PersonalizedTextInput.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import { TextInput, View, Text } from "react-native";
 
 export const PersonalizedTextInput: React.FC<InputProps> = (
-  props: InputProps,
+  props: InputProps
 ) => {
   const { placeholder, iconButton, inputInsideText, onChange, value } = props;
 
@@ -19,16 +19,18 @@ export const PersonalizedTextInput: React.FC<InputProps> = (
     <View style={inputStyle.input}>
       <TextInput
         style={{
-          flexGrow: 1,
+          flex: 1,
           ...generalStyle.basicText,
         }}
         onChangeText={handleChangeText}
         value={value}
         placeholder={placeholder}
         placeholderTextColor={primaryColors.lightGrey}
+        multiline={true}
+        scrollEnabled={true}
       />
+      <Text style={[generalStyle.basicText]}>{inputInsideText}</Text>
       {iconButton}
-      <Text style={generalStyle.basicText}>{inputInsideText}</Text>
     </View>
   );
 };
