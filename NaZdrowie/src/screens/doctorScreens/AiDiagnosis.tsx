1bfdc3f9af37b6ba780bec494a7e7cea3493585e
DroppedHard <szyper7@gmail.com>
2025-01-04 20:15:47 +0100

    Better UI when no results and health forms are present.

--- NaZdrowie/src/screens/doctorScreens/AiDiagnosis.tsx
+++ NaZdrowie/src/screens/doctorScreens/AiDiagnosis.tsx
@@ -10,9 +10,9 @@ import {
   useHealthFormData,
   usePatientData,
 } from "components/organisms/dataHooks";
-import { generalStyle, mainStyle } from "properties/styles";
+import { cardStyle, generalStyle, mainStyle } from "properties/styles";
 import React, { useCallback, useContext } from "react";
-import { ScrollView, SafeAreaView, Text } from "react-native";
+import { ScrollView, SafeAreaView, Text, View } from "react-native";
 import Toast from "react-native-toast-message";
 
 export const AiDiagnosis = ({
@@ -64,19 +64,32 @@ export const AiDiagnosis = ({
             queries={[patientResultsForAi, latestHealthForm]}
             temporaryTitle="Załączone badania"
             renderSuccess={([results, healthForm]) => (
-              <ListCard
-                title="Załączone badania"
-                data={[...healthForm, ...results]}
-                extraButton={
-                  <PrimaryButton
-                    title="Poproś AI o analizę"
-                    handleOnClick={() => {
-                      startAiDiagnosis();
-                      patientLatestPrediction.refetch();
-                    }}
+              <>
+                {healthForm.length + results.length !== 0 ? (
+                  <ListCard
+                    title="Załączone badania"
+                    data={[...healthForm, ...results]}
+                    extraButton={
+                      <PrimaryButton
+                        title="Poproś AI o analizę"
+                        handleOnClick={() => {
+                          startAiDiagnosis();
+                          patientLatestPrediction.refetch();
+                        }}
+                      />
+                    }
                   />
-                }
-              />
+                ) : (
+                  <View style={cardStyle.container}>
+                    <Text style={generalStyle.titleText}>
+                      Załączone badania
+                    </Text>
+                    <Text style={generalStyle.basicText}>
+                      Brak wyników badań i poprzednich formularzy zdrowia
+                    </Text>
+                  </View>
+                )}
+              </>
             )}
           />
           <QueryWrapper
